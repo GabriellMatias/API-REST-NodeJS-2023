51f964940e3018df204ac9f891f209bb204bcfde
GabriellMatias <gabriel.goiasat@gmail.com>
2023-05-09 17:42:59 -0300

    :hammer: Chore: Productions configs

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,8 +11,10 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3334),
+  /* coerce converte para numero pra passar no test */
+  PORT: z.coerce.number().default(3334),
 })
 
 const _env = envSchema.safeParse(process.env)
